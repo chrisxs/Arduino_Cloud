d44901d6855a02b4bbb6f5d1897ca20759c528f9
滑稽哥 <chrisxs@qq.com>
2023-03-10 13:22:19 +0800

    123123
    123

--- ESP8266_PHP_MySQL/ESP8266_BME280_PostData/src/main.cpp
+++ ESP8266_PHP_MySQL/ESP8266_BME280_PostData/src/main.cpp
@@ -1,25 +1,24 @@
 #ifdef ESP32
-  #include <WiFi.h>
-  #include <HTTPClient.h>
+#include <WiFi.h>
+#include <HTTPClient.h>
 #else
-  #include <ESP8266WiFi.h>
-  #include <ESP8266HTTPClient.h>
-  #include <WiFiClient.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClient.h>
 #endif
 
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h>
 
-const char* ssid = "你的WiFi-SSID";
-const char* password = "你的WiFi密码";
+const char *ssid = "chrisxs";
+const char *password = "aj20160223";
 
-//服务器URL
-const char* serverName = "http://XXX.com/esp-post-data.php";
-
+// 服务器URL
+const char *serverName = "http://chrisxs.com/web-images/esp-post-data.php";
 
 // apiKeyValue为随机生成，必须与esp-database.php内的API KEY保持一致
-String apiKeyValue = "你的API-KEY";
+String apiKeyValue = "hhzjd32jd94r";
 String sensorName = "BME280";
 String sensorLocation = "office";
 
@@ -31,24 +30,26 @@ String sensorLocation = "office";
 
 #define SEALEVELPRESSURE_HPA (1013.25)
 
-Adafruit_BME280 bme;  // I2C
-//Adafruit_BME280 bme(BME_CS);  // hardware SPI
-//Adafruit_BME280 bme(BME_CS, BME_MOSI, BME_MISO, BME_SCK);  // software SPI
+Adafruit_BME280 bme; // I2C
+// Adafruit_BME280 bme(BME_CS);  // hardware SPI
+// Adafruit_BME280 bme(BME_CS, BME_MOSI, BME_MISO, BME_SCK);  // software SPI
 
 // the following variables are unsigned longs because the time, measured in
 // milliseconds, will quickly become a bigger number than can be stored in an int.
 unsigned long lastTime = 0;
 // Timer set to 10 minutes (600000)
-//unsigned long timerDelay = 600000;
+// unsigned long timerDelay = 600000;
 // Set timer to 30 seconds (30000)
 unsigned long timerDelay = 30000;
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
 
   WiFi.begin(ssid, password);
   Serial.println("Connecting");
-  while(WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(500);
     Serial.print(".");
   }
@@ -58,19 +59,24 @@ void setup() {
 
   // (you can also pass in a Wire library object like &Wire2)
   bool status = bme.begin(0x76);
-  if (!status) {
+  if (!status)
+  {
     Serial.println("Could not find a valid BME280 sensor, check wiring or change I2C address!");
-    while (1);
+    while (1)
+      ;
   }
-  
+
   Serial.println("Timer set to 30 seconds (timerDelay variable), it will take 30 seconds before publishing the first reading.");
 }
 
-void loop() {
-  //Send an HTTP POST request every 10 minutes
-  if ((millis() - lastTime) > timerDelay) {
-    //Check WiFi connection status
-    if(WiFi.status()== WL_CONNECTED){
+void loop()
+{
+  // Send an HTTP POST request every 10 minutes
+  if ((millis() - lastTime) > timerDelay)
+  {
+    // Check WiFi connection status
+    if (WiFi.status() == WL_CONNECTED)
+    {
       HTTPClient http;
 
       // Your Domain name with URL path or IP address with path
@@ -80,39 +86,40 @@ void loop() {
       http.addHeader("Content-Type", "application/x-www-form-urlencoded");
 
       // Prepare your HTTP POST request data
-      String httpRequestData = "api_key=" + apiKeyValue + "&sensor=" + sensorName
-                            + "&location=" + sensorLocation + "&value1=" + String(bme.readTemperature())
-                            + "&value2=" + String(bme.readHumidity()) + "&value3=" + String(bme.readPressure()/100.0F) + "";
+      String httpRequestData = "api_key=" + apiKeyValue + "&sensor=" + sensorName + "&location=" + sensorLocation + "&value1=" + String(bme.readTemperature()) + "&value2=" + String(bme.readHumidity()) + "&value3=" + String(bme.readPressure() / 100.0F) + "";
       Serial.print("httpRequestData: ");
       Serial.println(httpRequestData);
 
       // You can comment the httpRequestData variable above
       // then, use the httpRequestData variable below (for testing purposes without the BME280 sensor)
-      //String httpRequestData = "api_key=tPmAT5Ab3j7F9&sensor=BME280&location=Office&value1=24.75&value2=49.54&value3=1005.14";
+      // String httpRequestData = "api_key=tPmAT5Ab3j7F9&sensor=BME280&location=Office&value1=24.75&value2=49.54&value3=1005.14";
 
       // Send HTTP POST request
       int httpResponseCode = http.POST(httpRequestData);
 
       // If you need an HTTP request with a content type: text/plain
-      //http.addHeader("Content-Type", "text/plain");
-      //int httpResponseCode = http.POST("Hello, World!");
+      // http.addHeader("Content-Type", "text/plain");
+      // int httpResponseCode = http.POST("Hello, World!");
 
       // If you need an HTTP request with a content type: application/json, use the following:
-      //http.addHeader("Content-Type", "application/json");
-      //int httpResponseCode = http.POST("{\"value1\":\"19\",\"value2\":\"67\",\"value3\":\"78\"}");
+      // http.addHeader("Content-Type", "application/json");
+      // int httpResponseCode = http.POST("{\"value1\":\"19\",\"value2\":\"67\",\"value3\":\"78\"}");
 
-      if (httpResponseCode>0) {
+      if (httpResponseCode > 0)
+      {
         Serial.print("HTTP Response code: ");
         Serial.println(httpResponseCode);
       }
-      else {
+      else
+      {
         Serial.print("Error code: ");
         Serial.println(httpResponseCode);
       }
       // Free resources
       http.end();
     }
-    else {
+    else
+    {
       Serial.println("WiFi Disconnected");
     }
     lastTime = millis();
