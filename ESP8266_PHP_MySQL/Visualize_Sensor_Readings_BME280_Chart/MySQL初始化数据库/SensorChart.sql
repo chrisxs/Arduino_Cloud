8560bc74e96ce8e0cc94c76efbc6fdca601056bf
滑稽哥 <chrisxs@qq.com>
2023-03-18 02:05:29 +0800

    Update SensorChart.sql

--- ESP8266_PHP_MySQL/Visualize_Sensor_Readings_BME280_Chart/MySQL初始化数据库/SensorChart.sql
+++ ESP8266_PHP_MySQL/Visualize_Sensor_Readings_BME280_Chart/MySQL初始化数据库/SensorChart.sql
@@ -1,7 +1,7 @@
-CREATE TABLE Sensor (
-    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    value1 VARCHAR(10),
-    value2 VARCHAR(10),
-    value3 VARCHAR(10),
-    reading_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+CREATE TABLE SensorChart (
+    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, -- 定义 id 列为整数类型，自动递增并作为主键
+    value1 VARCHAR(10), -- 定义 value1 列为可存储最长 10 个字符的字符串类型
+    value2 VARCHAR(10), -- 定义 value2 列为可存储最长 10 个字符的字符串类型
+    value3 VARCHAR(10), -- 定义 value3 列为可存储最长 10 个字符的字符串类型
+    reading_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP -- 定义 reading_time 列为时间戳类型，设置默认值为当前时间，每次更新时自动更新时间戳
 )
